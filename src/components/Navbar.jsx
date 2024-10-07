d36c6750efdb46f9de4b3dbb2633a95808f88a94
shivam01 <shivamnamdev32@gmail.com>
2024-10-07 18:32:16 +0530

    Weekly assignment 4 Diroctory app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-2 shadow-md mb-3">
+    <div className="container mx-auto">
+      <h1 className="text-black text-xl font-bold text-center">Shivam Namdev Directory App</h1>
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
